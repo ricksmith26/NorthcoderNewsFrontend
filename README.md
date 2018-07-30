5ca81ccd2a0e7fd1ed82f5bd594c0b9fe7e449d7
ricksmith26 <ricksmith26@hotmail.com>
2018-07-30 16:02:12 +0100

    readme added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #FE-FT-NC-News
 
-In this sprint I will create a Northcoders News app with react frontend and use my mongodb backend api that I have previously created. Please click to following link to view 'https://github.com/ricksmith26/copyofNCNEWSFRONT.git'
+In this sprint I will create a Northcoders News app with react frontend and use my mongodb backend api that I have previously created. Please click to following link to view 'https://ncnewsfrontend.herokuapp.com/'
 
 #Getting Started
 
@@ -18,10 +18,12 @@ To run this git on you machine git clone https://github.com/ricksmith26/copyofNC
     react-router-dom ^4.3.1,
     react-scripts 1.1.4
 
+Once installed enter 'npm start' in your commandline
+
 #Error handling
 
 I have used Cypress for the error handling in frontend although I have still to add much of this.
 
 #Author
 
-The front and backend for this app has been written by Rick Smith
+The front and backend for this app has been written by Rick Smith.
--- cypress/integration/articles.spec.js
+++ cypress/integration/articles.spec.js
@@ -1,15 +1,15 @@
-describe.only('ArtcilesView', () => {
+describe('ArtcilesView', () => {
   beforeEach(() => {
     cy.visit('/');
   });
   it('will accept username in he login box', () => {
     cy.get('.inputUsername')
-      .type('usernameInput')
+      .type('usernameInput', { force: true })
       .should('have.value', 'usernameInput');
   });
   it('will accept a password in the password input box', () => {
     cy.get('.inputPassword')
-      .type('userPassword')
+      .type('userPassword', { force: true })
       .should('have.value', 'userPassword');
   });
   it('articles length should be 36', () => {
@@ -18,11 +18,32 @@ describe.only('ArtcilesView', () => {
   it('amount of options on the drop down bar shuld be 4 inc a blank one', () => {
     cy.get('.dropdown').should('have.length', 4);
   });
-  it('returns the articles froma certain topic', () => {
+  it('returns the articles from the cooking topic', () => {
     cy.get('select')
-
-      .select('cooking')
+      .select('cooking', { force: true })
       .invoke('val')
+      .get('.articleThumbnail')
       .should('have.length', 12);
+  });
+  it('returns the articles from the football topic', () => {
+    cy.get('select')
+      .select('football', { force: true })
+      .invoke('val')
+      .get('.articleThumbnail')
+      .should('have.length', 12);
+  });
+  it('returns the articles from the coding topic', () => {
+    cy.get('select')
+      .select('coding', { force: true })
+      .invoke('val')
+      .get('.articleThumbnail')
+      .should('have.length', 12);
+  });
+  it('take the user to the correct article when clicking on a title', () => {
+    cy.get('.articleThumbnail')
+      .first()
+      .click('topLeft', { force: true })
+      .url()
+      .should('eq', 'http://localhost:3000/articles/5b57353ef9af5955f794b0b5');
   });
 });
